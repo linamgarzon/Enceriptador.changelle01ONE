d8ed22c393cc907afb86bbfac1ad595cf64101ff
linamgarzon <118136883+linamgarzon@users.noreply.github.com>
2023-01-27 15:09:18 -0500

    Arreglo caracteres mayusculas encriptacion

--- encriptado1.js
+++ encriptado1.js
@@ -1,40 +1,64 @@
 var enviado;
 var para_copiar;
-var letras='abcdefghijklmnñopqrstuvwxyz';
+var letras='abcdefghijklmnñopqrstuvwxyz ';
+var miarray=new Array();
+var mayus;
 function verificar(){
    var texto_enviado=document.getElementById('texto').value;
    enviado=texto_enviado;
 }
-function encriptar(){
+function VerificarMayus(){
+    console.log(enviado)
     for(var i=0; i<enviado.length;i++){
-        if(letras.indexOf(enviado.charAt(i),0)==-1){
-            alert('Tienes una letra en mayúscula o con acento');
-            ingresodetexto.reset();
-        }
-        else if(letras.indexOf(enviado.charAt(i))!=-1){
-            var textCifrado=enviado.replaceAll('a','ai').replaceAll('e','enter').replaceAll('i','imes').replaceAll('o','ober').replaceAll('u','ufat');
-            document.getElementById('mucheco').style.display='none';
-            document.getElementById('mensaje').innerHTML='';
-            textoEncrip=document.getElementById('encriptado').innerHTML=textCifrado;
-            var copiado=document.getElementById('copiar').style.display='block';
-            ingresodetexto.reset();
-        }
+         miarray.push(letras.indexOf(enviado.charAt(i),0))
+         console.log(miarray)
+    }
+    mayus=miarray.find(elemento => elemento == -1);
+    console.log(mayus);
+    while(miarray.length>0){
+        miarray.pop();
+    }
+    if(mayus==-1){
+        alert('Tienes mayusculas o acentos en tu texto');
+        ingresodetexto.reset();
+    }
+    else{
+        encriptar();
     }
 }
-function desemcriptar(){
+function Verificaracentos(){
+    console.log(enviado)
     for(var i=0; i<enviado.length;i++){
-        if(letras.indexOf(enviado.charAt(i),0)==-1){
-            alert('Tienes una letra en mayúscula o letra con acento');
-            ingresodetexto.reset();
-        }
-    else if(letras.indexOf(enviado.charAt(i))!=-1){
+         miarray.push(letras.indexOf(enviado.charAt(i),0))
+         console.log(miarray)
+    }
+    mayus=miarray.find(elemento => elemento == -1);
+    console.log(mayus);
+    while(miarray.length>0){
+        miarray.pop();
+    }
+    if(mayus==-1){
+        alert('Tienes mayusculas o acentos en tu texto');
+        ingresodetexto.reset();
+    }
+    else{
+        desemcriptar();
+    }
+}
+function encriptar(){
+    var textCifrado=enviado.replaceAll('e','enter').replaceAll('i','imes').replaceAll('o','ober').replaceAll('u','ufat').replaceAll('a','ai');
+    document.getElementById('mucheco').style.display='none';
+    document.getElementById('mensaje').innerHTML='';
+    textoEncrip=document.getElementById('encriptado').innerHTML=textCifrado;
+    var copiado=document.getElementById('copiar').style.display='block';
+    ingresodetexto.reset();
+}
+function desemcriptar(){
     var textCifrado=enviado.replaceAll('ai','a').replaceAll('enter','e').replaceAll('imes','i').replaceAll('ober','o').replaceAll('ufat','u');
     document.getElementById('mucheco').style.display='none';
     document.getElementById('mensaje').innerHTML='';
     document.getElementById('encriptado').innerHTML=textCifrado;
     ingresodetexto.reset();
-    }
-}
 }
 function copiar(){
     var encriptado=document.getElementById('encriptado');
